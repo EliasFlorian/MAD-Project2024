d3b763ae282569983099a56d364536b0e7b98f5c
EliasFlorian <elias.damisch@gmail.com>
2024-06-24 16:28:26 +0200

    homescreen adapted

--- app/src/main/java/com/example/mad_project2024/screens/HomeScreen.kt
+++ app/src/main/java/com/example/mad_project2024/screens/HomeScreen.kt
@@ -3,9 +3,14 @@ package com.example.mad_project2024.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mad_project2024.R
@@ -14,43 +19,100 @@ import com.example.mad_project2024.ui.theme.MovieAppMAD24Theme
 
 @Composable
 fun HomeScreen() {
-    ModeCard()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        ModeCard(title = stringResource(R.string.travel_mode), description = stringResource(R.string.description_travel_mode))
+        Spacer(Modifier.size(64.dp))
+        ModeCard(title = stringResource(R.string.general_mode), description = stringResource(R.string.description_general_mode))
+    }
+
 
 }
 
 @Composable
-fun ModeCard(
+fun ModeCard(title: String, description: String, modifier:Modifier = Modifier) {
 
-    modifier:Modifier = Modifier) {
-    Card(
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
-        modifier = modifier,
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
     ) {
-        Column(
+        Text(
+            text = title,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .sizeIn(minHeight = 72.dp)
-        ) {
+                .padding(8.dp),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.displaySmall
+        )
 
-            Text(
-                text = stringResource(R.string.travel_mode),
-                style = MaterialTheme.typography.displaySmall
-            )
+        Spacer(Modifier.size(8.dp))
 
-            Spacer(Modifier.size(8.dp))
-
-                Text(
-                    text = stringResource(R.string.description_travel_mode),
-                    //style = MaterialTheme.typography.displaySmall
-                )
-
-
-
-
-        }
+        Text(
+            text = description,
+            modifier = Modifier.padding(8.dp),
+            textAlign = TextAlign.Center,
+            color = Color.Black
+        )
     }
+
 }
+
+
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun CenterAlignedTopAppBarExample() {
+//    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+//
+//    Scaffold(
+//        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+//
+//        topBar = {
+//            CenterAlignedTopAppBar(
+//                colors = TopAppBarDefaults.topAppBarColors(
+//                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+//                    titleContentColor = MaterialTheme.colorScheme.primary,
+//                ),
+//                title = {
+//                    Text(
+//                        "Centered Top App Bar",
+//                        maxLines = 1,
+//                        overflow = TextOverflow.Ellipsis
+//                    )
+//                },
+//                navigationIcon = {
+//                    IconButton(onClick = { /* do something */ }) {
+//                        Icon(
+//                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+//                            contentDescription = "Localized description"
+//                        )
+//                    }
+//                },
+//                actions = {
+//                    IconButton(onClick = { /* do something */ }) {
+//                        Icon(
+//                            imageVector = Icons.Filled.Menu,
+//                            contentDescription = "Localized description"
+//                        )
+//                    }
+//                },
+//                scrollBehavior = scrollBehavior,
+//            )
+//        },
+//    ) { innerPadding ->
+//        ScrollContent(innerPadding)
+//    }
+//}
+
+
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
